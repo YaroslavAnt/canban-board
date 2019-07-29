a526691ecaeb7a2edcd42fba86b46cbed9a0462d
YaroslavAnt <antonchyk.yar@gmail.com>
2019-07-29 11:26:01 +0300

    initial commit

---
+++ src/redux/reducers/column.js
@@ -0,0 +1,20 @@
+﻿import * as actionTypes from "../actions/actionTypes";
+
+const initialState = [];
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+
+
+        case actionTypes.SET_AUTH_REDIRECT_PATH:
+            return {
+                ...state,
+                authRedirectPath: action.path
+            };
+
+        default:
+            return state;
+    }
+};
+
+export default reducer;
